d4094eeb2cfb988f9bcca01878986d6ed1f3cb3b
Nate Allen <email@ncallen.com>
2020-01-23 12:53:04 -0500

    Set the timezone correctly and fix icon

--- src/action.js
+++ src/action.js
@@ -1,6 +1,6 @@
-const log = require('loglevel')
-const fs = require('fs')
-const request = require('request')
+const log = require('loglevel');
+const fs = require('fs');
+const request = require('request');
 
 if ( ! process.env.GITHUB_WORKSPACE ) {
 	require('dotenv').config({path:'.env'});
@@ -9,13 +9,23 @@ if ( ! process.env.GITHUB_WORKSPACE ) {
 let contents = fs.readFileSync("/action/template-start.json");
 let json     = JSON.parse(contents);
 const moment = require('moment');
+const tz     = require('moment-timezone');
 
 if ( ! process.env.SITE_IMAGE_URL ) {
 	process.env.SITE_IMAGE_URL = 'https://raw.githubusercontent.com/linchpin/action-slack-notification/master/images/default-client-image.png';
 }
 
 if ( ! process.env.NOTIFICATION_ICON ) {
-	process.env.NOTIFICATION_ICON = 'https://raw.githubusercontent.com/linchpin/action-slack-notification/master/images/in-progress.png';
+	switch( process.env.BUILD_STATUS ) {
+	  case 'completed':
+	    process.env.NOTIFICATION_ICON = 'https://raw.githubusercontent.com/linchpin/action-slack-notification/master/images/success.png';
+	    break;
+	  case 'failed':
+	    process.env.NOTIFICATION_ICON = 'https://raw.githubusercontent.com/linchpin/action-slack-notification/master/images/fail.png';
+	    break;
+	  default:
+	    process.env.NOTIFICATION_ICON = 'https://raw.githubusercontent.com/linchpin/action-slack-notification/master/images/in-progress.png';
+	}
 }
 
 const textMap = {
@@ -27,7 +37,7 @@ const textMap = {
   SITE_URL_SHORT: process.env.SITE_URL.replace(/(^\w+:|^)\/\//, ''),
   COMMITTER: process.env.GITHUB_ACTOR,
   SITE_IMAGE_URL: process.env.SITE_IMAGE_URL,
-  TIMESTAMP: moment().format('MMMM Do YYYY, h:mm:ss A'),
+  TIMESTAMP: moment().tz('America/New_York').format('MMMM Do YYYY, h:mm:ss A'),
   CHANNEL_ID: process.env.CHANNEL_ID
 }
 
