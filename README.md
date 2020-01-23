5f671d46a46dbd6e80d4f7004d15709e9b1d1160
Nate Allen <email@ncallen.com>
2020-01-23 10:02:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Slack Build Notification
+
+This action sends a notification when a build starts, finishes, or fails.
+
+## Developing locally
+
+All the code is under the `src` folder:
+ 1. `cd src`
+ 2. `npm install`
+
+After the project is setup you can just run with: 
+
+```bash
+GITHUB_EVENT_PATH="sample-payload.json" node action.js
+```
+
+Note: `GITHUB_EVENT_PATH` is an [environment variable in Actions](https://developer.github.com/actions/creating-github-actions/accessing-the-runtime-environment/#environment-variables) where the event payload is stored for analysis during execution. For local testing there's a file `src/sample-payload.json` which stores the sample payload of a `push` event. If you're planning on creating a workflow that triggers on another type of event, you can just [fetch a sample payload](https://developer.github.com/v3/activity/events/types) and replace the contents of that file.  
---
+++ src/action.js
@@ -0,0 +1,61 @@
+const log = require('loglevel')
+const fs = require('fs')
+const request = require('request')
+
+if ( ! process.env.GITHUB_WORKSPACE ) {
+	require('dotenv').config({path:'.env'});
+}
+
+let contents = fs.readFileSync("template.json");
+let json     = JSON.parse(contents);
+const moment = require('moment');
+
+if ( process.env.NOTIFICATION_ICON ) {
+
+} else {
+
+}
+
+const textMap = {
+  NOTIFICATION_ICON: process.env.NOTIFICATION_ICON,
+  BUILD_STATUS: process.env.BUILD_STATUS,
+  BUILD_STATUS_MESSAGE: process.env.BUILD_STATUS_MESSAGE,
+  ENVIRONMENT: process.env.ENVIRONMENT,
+  SITE_URL: process.env.SITE_URL,
+  SITE_URL_SHORT: process.env.SITE_URL.replace(/(^\w+:|^)\/\//, ''),
+  COMMITTER: process.env.GITHUB_ACTOR,
+  SITE_IMAGE_URL: process.env.SITE_IMAGE_URL,
+  TIMESTAMP: moment().format('MMMM Do YYYY, h:mm:ss A'),
+  CHANNEL_ID: process.env.CHANNEL_ID
+}
+
+const parse = (template, textMap) => {
+  let output = template
+
+  for (let [id, text] of Object.entries(textMap)) {
+    output = output.replace(new RegExp(`\\$\{${id}}`, 'mg'), text)
+  }
+
+  return output
+}
+
+const parsed = parse(contents.toString(), textMap)
+
+var url = "https://slack.com/api/chat.postMessage";
+
+var headers = {
+   "Authorization": "Bearer " + process.env.SLACK_BOT_TOKEN,
+   "Content-Type" : "application/json"
+}
+
+request.post({
+   "url": url,
+   "headers": headers,
+   "body": parsed
+}, (err, response, body) => {
+   if (err) {
+       reject(err);
+   }
+   console.log("response: ", JSON.stringify(response));
+   console.log("body: ",body);
+});
