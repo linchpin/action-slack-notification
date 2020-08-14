2d81d36d5adddc7d99ee1c4e949a95aef8390e9c
Nate Allen <email@ncallen.com>
2020-08-14 11:46:45 -0400

    Check if there are any changes to the changelog before trying to access the array

--- src/action.js
+++ src/action.js
@@ -28,6 +28,10 @@ if ( process.env.BUILD_STATUS === 'completed' ) {
 	const regex = /#+\s?Changelog\s*(?:(?:#{2,})\s(v\d\.\d(?:\.\d)?))([\s\S]*?)((?:(?:#+)\s(?:v\d\.\d(?:\.\d)?))|#+|$){1}/;
 	const changes = readme_file.match(regex);
 
+	if ( ! changes ) {
+		return;
+	}
+
 	if ( (typeof changes[1] !== 'undefined' && 'refs/tags/' + changes[1] === event.ref ) && ( typeof changes[2] !== 'undefined' ) ) {
 		json.blocks.push( {
 			"type": "divider"
